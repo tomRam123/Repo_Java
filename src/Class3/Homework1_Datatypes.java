883d938a56cfebfe95a7366fd1cab91ff51f5ebb
Tomas Ramirez <you@example.com>
2021-11-04 22:38:12 -0400

    Changes To Datatypes File

--- src/Class3/Homework1_Datatypes.java
+++ src/Class3/Homework1_Datatypes.java
@@ -1,5 +1,13 @@
 package Class3;
 
 public class Homework1_Datatypes {
+// i dont know what im doing, hahaha
 
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+
+    
 }
+
