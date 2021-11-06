1b95d8e472d67833a01af56d7bf25fc8e61f86f4
Tomas Ramirez <you@example.com>
2021-11-05 23:39:14 -0400

    just practicing

--- src/Class3/Homework1_Datatypes.java
+++ src/Class3/Homework1_Datatypes.java
@@ -2,6 +2,7 @@ package Class3;
 
 public class Homework1_Datatypes {
 // i dont know what im doing, hahaha
+    //abcdefg
 
     public static void main(String[] args) {
         System.out.println("Hello World");
