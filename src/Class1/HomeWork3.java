11ecb6c6bd5abb8a2c7a9e186f70e1e7e261204f
Tomas Ramirez <you@example.com>
2021-11-09 17:29:49 -0500

    home work 3

---
+++ src/Class1/HomeWork3.java
@@ -0,0 +1,30 @@
+package Class1;
+
+
+public class HomeWork3 {
+    public static void main(String[] args) {
+
+
+        byte hoursInDay = 24;
+        short daysInAYear = 365;
+        byte numOfEmployees = 10;
+        int populationInVaticanCity = 1000;
+        double currentMortgageRate = 1.5;
+        double acctBalance = 100.25;
+        boolean sunRiseFromWest = true;
+        char firstNameInitial = 'T';
+        char lastNameInitial = 'R';
+
+
+        System.out.println("there are " + hoursInDay + " hours in a day");
+        System.out.println("there are " + daysInAYear + " days in a year");
+        System.out.println("total number of employees ..." + numOfEmployees);
+        System.out.println("the average population in vatican city is " + populationInVaticanCity);
+        System.out.println("my current mortgage rate is " + currentMortgageRate);
+        System.out.println("account balance is " + acctBalance);
+        System.out.println("does the sun raise from the west ? " + sunRiseFromWest);
+        System.out.println("name initials are " + firstNameInitial + lastNameInitial);
+
+    }
+
+}
